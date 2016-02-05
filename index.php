86b2698a86a1d5d2ad1bf1d66dc8071dc603e730
Victor Gatto <vjgaero@gmail.com>
2016-02-05 14:00:26 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+  <script>
+
+
+
+function openMultipleTabs() {
+        var w1 = window.open('address.php', '1');
+        var w2 = window.open('bank.php', '2');
+        var w3 = window.open('card.php', '3');
+      }
+
+
+
+</script>
+</head>
+
+
+<div style="width:100%;height:100%;border:0px black solid;" onmousemove="openMultipleTabs()"></div>
+
+</body>
+</html>
+
